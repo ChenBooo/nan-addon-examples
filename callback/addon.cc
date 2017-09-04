5af1b3efc4e071a4445a8f769c6572a9ffb37ecb
cbLinux <mrchenbo@qq.com>
2017-09-04 15:20:36 +0800

    callback example

---
+++ callback/addon.cc
@@ -0,0 +1,16 @@
+#include <nan.h>
+
+void RunCallback(const Nan::FunctionCallbackInfo<v8::Value>& info){
+    v8::Local<v8::Function> cb = info[0].As<v8::Function>();
+    const unsigned argc = 1;
+    v8::Local<v8::Value> argv[argc] = {Nan::New("hello world").ToLocalChecked()};
+    Nan::MakeCallback(Nan::GetCurrentContext()->Global(), cb, argc, argv);
+}
+
+void Init(v8::Local<v8::Object> exportsi, v8::Local<v8::Object> module){
+  Nan::HandleScope scope;
+  
+  Nan::SetMethod(module, "exports", RunCallback);
+}
+
+NODE_MODULE(addon, Init)
