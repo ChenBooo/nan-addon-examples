3db0ec3e6217324a8fb7a125e4e38916b5faf9ab
cbLinux <mrchenbo@qq.com>
2017-09-04 13:55:05 +0800

    hello world example

---
+++ helloworld/hello.cc
@@ -0,0 +1,13 @@
+#include <nan.h>
+
+void Method(const Nan::FunctionCallbackInfo<v8::Value>& info){
+  info.GetReturnValue().Set(Nan::New<v8::String>("world").ToLocalChecked());
+}
+
+void Init(v8::Local<v8::Object> exports){
+  Nan::HandleScope scope;
+  
+  Nan::SetMethod(exports, "hello", Method);
+}
+
+NODE_MODULE(hello, Init)
