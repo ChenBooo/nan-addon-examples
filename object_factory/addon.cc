8eedda57d1dbf3ef9f256cf226adbda21fc3a73b
cbLinux <mrchenbo@qq.com>
2017-09-04 15:39:12 +0800

    object factory example

---
+++ object_factory/addon.cc
@@ -0,0 +1,16 @@
+#include <nan.h>
+
+void CreateObject(const Nan::FunctionCallbackInfo<v8::Value>& info){
+  v8::Local<v8::Object> obj = Nan::New<v8::Object>();
+  obj->Set(Nan::New("msg").ToLocalChecked(), info[0]->ToString());
+
+  info.GetReturnValue().Set(obj);
+}
+
+void Init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module){
+  Nan::HandleScope scope;
+  
+  Nan::SetMethod(module, "exports", CreateObject);
+}
+
+NODE_MODULE(addon, Init)
